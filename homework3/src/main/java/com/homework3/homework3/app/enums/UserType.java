908289e53476b99db7809c873cd163775d7d2c7f
DilberASLAN <dilberraslann@gmail.com>
2022-03-03 22:05:06 +0300

    Edit

--- homework3/src/main/java/com/homework3/homework3/app/enums/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.homework3.homework3.app.enums;
-
-public enum UserType {
-    PERSON,
-    CORPORATION
-}
--- homework3/src/main/java/com/homework3/homework3/gen/entity/BaseModel.java
+++
@@ -1,5 +0,0 @@
-package com.homework3.homework3.gen.entity;
-
-public interface BaseModel {
-    Long getId();
-}
